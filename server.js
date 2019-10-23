4a3c1b4bca9022417080a4324b613b6e1a9e0ca9
Heypietro <pietro_96@outlook.it>
2019-10-24 05:45:44 +1100

    new mongo connection

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const app = express();
 
@@ -10,12 +12,20 @@ app.use(bodyParser.json());
 // DB config
 const db = require("./config/keys").mongoURI;
 
-// Connect to mongo db
-mongoose
-	.connect(db).then(
-		() => console.log("MongoDB connected.."),
-	    err => console.log(err)
-	);
+// Connect to mongo db 
+
+// Database Name
+const dbName = 'test';
+
+// Use connect method to connect to the server
+MongoClient.connect(db, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
 	
 const port = process.env.PORT || 5000;
 
