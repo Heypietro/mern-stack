65a9bbc9b8f4d0796df0accd51b63b76b2f4419b
Heypietro <pietro_96@outlook.it>
2019-10-24 05:21:28 +1100

    new mongose update

--- server.js
+++ server.js
@@ -12,9 +12,16 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to mongo db
 mongoose
-	.connect(db)
-	.then(() => console.log("MongoDB connected.."))
-	.catch(err => console.log(err));
+	.connect(db);
+	//.then(() => console.log("MongoDB connected.."))
+	//.catch(err => console.log(err));
+	
+var MyModel = mongoose.model('Test', new Schema({ name: String }));
+// Works
+MyModel.findOne((error, result) => {
+	console.log(`This is error ${error}`);
+	console.log(`This is the result ${result}`);
+});
 	
 const port = process.env.PORT || 5000;
 
