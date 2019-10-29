b0e53803edadcbbc0d995408afcfb8673d5bc394
Heypietro <pietro_96@outlook.it>
2019-10-29 22:32:19 +1100

    new modify

---
+++ routes/api/items.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const Item = require("../../models/Item");
+
+router.get("/", (req,res) =>{
+	Item.find()
+		then(items => res.json(items));
+});
+
+module.exports = router;
